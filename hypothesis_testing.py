1bfb74d5614c6ebe5d05a30623ca3b83a651f174
Matt Santos <mattsantos541@gmail.com>
2019-10-07 08:44:55 -0500

    Hypothesis

---
+++ hypothesis_testing.py
@@ -0,0 +1,8 @@
+##Hypothesis
+
+#Do your work for this exercise in a jupyter notebook named hypothesis_testing.ipynb.
+#For each of the following questions, formulate a null and alternative hypothesis (be as specific as you can be), then give an example of what a true positive, true negative, type I and type II errors would look like.
+#is the website redesign any good?
+Is the website redesign any good?
+H0: Web traffic after the redesign did not increase.
+Ha: Web traffic after the redesign increased.
--- probability_distributions.py
+++ probability_distributions.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 from scipy import stats
-from scipy import uniform
+
 
 #A bank found that the average number of cars waiting during the noon hour at a drive-up window follows a Poisson distribution with a mean of 2 cars. Make a chart of this distribution and answer these questions concerning the probability of cars waiting at the drive-up window.
 
@@ -20,7 +20,7 @@ waiting.cdf(1)
 gpa=stats.norm(3, .3)
 gpa.rvs(10000).mean()
 #What grade point average is required to be in the top 5% of the graduating class?
-gpa.sf(.95)
+gpa.isf(.05)
 
 #What GPA constitutes the bottom 15% of the class?
 gpa.ppf(.15)
@@ -33,7 +33,7 @@ clicks.pmf(97)
 #You are working on some statistics homework consisting of 100 questions where all of the answers 
 #are a probability rounded to the hundreths place. Looking to save time, you put down random probabilities 
 #as the answer to each question.
-hw= stats.binom(100, .01)
+hw= stats.binom(0, .01)
 
 hw.sf(.6)
 
@@ -47,21 +47,22 @@ students.sf(5)
 
 #You want to get lunch at La Panaderia, but notice that the line is usually very long at lunchtime. After several weeks of careful observation, you notice that the average number of people in line when your lunch break starts is normally distributed with a mean of 15 and standard deviation of 3. If it takes 2 minutes for each person to order, and 10 minutes from ordering to getting your food, what is the likelihood that you have at least 15 minutes left to eat your food before you have to go back to class? Assume you have one hour for lunch, and ignore travel time to and from La Panaderia.
 people_in_line = stats.norm(15, 3)
-lunch_hour= (60-15-10)//2
+lunch_hour= (60-(15+10+2)
 people_in_line.cdf(lunch_hour)
 
 #Connect to the employees database and find the average salary of current employees, along with the standard deviation. Model the distribution of employees salaries with a normal distribution and answer the following questions:
-
-#What percent of employees earn less than 60,000?
-#What percent of employees earn more than 95,000?
-#What percent of employees earn between 65,000 and 80,000?
-#What do the top 5% of employees make?
-
 salaries = pd.read_sql(query,url)
 mean = salaries['salary'].mean()
 std = salaries['salary'].std
 salary_dist = stats.norm(mean,std)
+
+#What percent of employees earn less than 60,000?
 p_under_60k = salary_dist.cdf(60_000)
+
+#What percent of employees earn more than 95,000?
 p_over_95k = salary_dist.sf(95_000)
+
+#What percent of employees earn between 65,000 and 80,000?
 p_between_65k_and_80k = salary_dist.cdf(80_000) - salary_dist.cdf(65_000)
+#What do the top 5% of employees make?
 top_5_percent = salary_dist.isf(.05)
