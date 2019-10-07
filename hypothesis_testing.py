c41d6399db57c4534ca31cc6747563a6b7f1fe37
Matt Santos <mattsantos541@gmail.com>
2019-10-07 10:55:19 -0500

    Complete

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -2,7 +2,23 @@
 
 #Do your work for this exercise in a jupyter notebook named hypothesis_testing.ipynb.
 #For each of the following questions, formulate a null and alternative hypothesis (be as specific as you can be), then give an example of what a true positive, true negative, type I and type II errors would look like.
+
 #is the website redesign any good?
-Is the website redesign any good?
-H0: Web traffic after the redesign did not increase.
-Ha: Web traffic after the redesign increased.
+H0: web traffic did not increase after the web site design
+H1: the website design did increase web traffic
+Type 1: the increase in traffice can be attributed to the redesign, when it is just a natural bump in traffic
+Type 2: the redesign did not increase traffice, when it was just a low traffic day
+
+#Is our television ad driving more sales?
+H1: there is no increase in sales after the ad aired
+HO: there is an increase in sales after the ad aired
+Type1: Sales increased bc of the ad, when it was just an increase in sales not related to the add 
+Type 2:Sales did not increase bc of the ad, when it eas just low sales
+
+#Has the network latency gone up since we switched internet service providers?
+HO: There is no change in latenxcy since we switched
+H1: There is an increase in network latency since we swithched
+
+Type 1: since we switched network latency has increased, but the sample is just high
+Type 2: There is no change in latency since we changed, but the sample just hsow no change
+
