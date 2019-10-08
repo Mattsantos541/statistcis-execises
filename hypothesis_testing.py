010191c6e66c276cf68c9e34670febe641993ef4
Matt Santos <mattsantos541@gmail.com>
2019-10-08 09:05:50 -0500

    CHi #3

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -22,3 +22,32 @@ H1: There is an increase in network latency since we swithched
 Type 1: since we switched network latency has increased, but the sample is just high
 Type 2: There is no change in latency since we changed, but the sample just hsow no change
 
+#Use the following contingency table to help answer the question of whether using a macbook and being a codeup 
+#student are independent of each other.
+
+index = ['Use MB', 'No MB']
+columns = ['Codeup', 'not Codeup']
+observed = pd.DataFrame([[49, 20], [1, 30]], index=index, columns=columns)
+n = observed.values.sum()
+expected = pd.DataFrame([[.49, .2], [.01, .30]], index=index, columns=columns) * n
+chi2 = ((observed - expected)**2 / expected).values.sum()
+nrows, ncols = observed.shape
+
+degrees_of_freedom = (nrows - 1) * (ncols - 1)
+
+p = stats.chi2(degrees_of_freedom).sf(chi2)
+
+print('Observed')
+print(observed)
+print('---\nExpected')
+print(expected)
+print('---\n')
+print(f'chi^2 = {chi2:.4f}')
+print(f'p     = {p:.4f}')
+
+#Choose another 2 categorical variables from the mpg dataset and perform a 
+c
+h
+i
+2
+contingency table test with them. Be sure to state your null and alternative hypotheses.
